e9f7580b840a5aaebb7b61064d4f31147f49daaa
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-03-06 22:13:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App'; // Make sure this path is correct based on your folder structure
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
