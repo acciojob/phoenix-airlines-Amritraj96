b7d541353732ac55329f664dbe4c6834e1685868
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-03-06 22:17:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
-
 import React from "react";
-import './../styles/App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import Home from "./Home";
+import FlightSearch from "./FlightSearch";
+import FlightBooking from "./FlightBooking";
+import Confirmation from "./Confirmation";
+import '../styles/App.css';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Provider store={store}>
+      <Router>
+        <div>
+          <header style={{ backgroundColor: '#3f51b5', color: 'white', padding: '15px' }}>
+            <h2 style={{ margin: 0 }}>Flight Booking App</h2>
+          </header>
+          <div className="container" style={{ padding: '20px' }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/flight-search" element={<FlightSearch />} />
+              <Route path="/flight-booking" element={<FlightBooking />} />
+              <Route path="/confirmation" element={<Confirmation />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </Provider>
+  );
+};
 
-export default App
+export default App;
